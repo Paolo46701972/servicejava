42a8c10766c1275bcf6938606dc1c252b4f8283a
Tryyy <paolocasasolaesteban@gmail.com>
2022-03-03 16:49:00 -0500

    Service v0.0.2 - Main

---
+++ src/main/java/pe/com/bcp/jms/colasmq/ColasMqApplication.java
@@ -0,0 +1,13 @@
+package pe.com.bcp.jms.colasmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColasMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColasMqApplication.class, args);
+	}
+
+}
